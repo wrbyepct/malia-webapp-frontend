df70540b65173dc9d1924dfe9f490f57c196f9fe
Furthermore <wrbyepcr11@gmail.com>
2023-12-05 15:58:53 +0800

    add base url to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/malia-webapp-frontend/',
   plugins: [react()],
   build: {
     outDir: 'build', // Add this line to specify the output directory
